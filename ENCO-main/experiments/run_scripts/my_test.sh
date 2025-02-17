cfed84981934377382d2f720e47bf729afc24c32
wufan <74455025+xujianyuan611@users.noreply.github.com>
2025-02-17 21:56:32 +0800

    Delete ENCO-main directory

--- ENCO-main/experiments/run_scripts/my_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Graphs are dynamically generated here to save disk memory
-
-cd ../  # Go back to experiment direction
-# Graph with 100 variables
-python run_exported_graphs.py --graph_files ../hihi.pt
